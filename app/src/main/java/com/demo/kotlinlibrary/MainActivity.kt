1a94cfc9e02a9131d8c5713887cd859e71ae9161
haresh002 <addon.haresh002@gmail.com>
2021-12-17 18:17:11 +0530

    second

--- app/src/main/java/com/demo/kotlinlibrary/MainActivity.kt
+++ app/src/main/java/com/demo/kotlinlibrary/MainActivity.kt
@@ -2,7 +2,6 @@ package com.demo.kotlinlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.demo.toast.CustomToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
