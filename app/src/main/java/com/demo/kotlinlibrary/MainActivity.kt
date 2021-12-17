a6f4ce61b81b2c64209780e072a2e80597f89fe5
haresh002 <addon.haresh002@gmail.com>
2021-12-17 16:54:18 +0530

    initial

---
+++ app/src/main/java/com/demo/kotlinlibrary/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.demo.kotlinlibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.toast.CustomToast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CustomToast.plus(1,2)
+    }
+}
---
+++ toast/src/main/java/com/demo/toast/CustomToast.kt
@@ -0,0 +1,12 @@
+package com.demo.toast
+
+class CustomToast {
+
+    companion object{
+
+        fun plus(a:Int,b:Int):Int
+        {
+            return a+b;
+        }
+    }
+}
