e218eba2a0578d966410470b17c2ac997ed39476
haresh002 <addon.haresh002@gmail.com>
2021-12-21 14:01:38 +0530

    toast massege

--- app/src/main/java/com/demo/kotlinlibrary/MainActivity.kt
+++ app/src/main/java/com/demo/kotlinlibrary/MainActivity.kt
@@ -2,11 +2,13 @@ package com.demo.kotlinlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo.toast.CustomToast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        CustomToast.showToast(this,"show Toast")
     }
 }
--- toast/src/main/java/com/demo/toast/CustomToast.kt
+++ toast/src/main/java/com/demo/toast/CustomToast.kt
@@ -1,5 +1,8 @@
 package com.demo.toast
 
+import android.content.Context
+import android.widget.Toast
+
 class CustomToast {
 
     companion object{
@@ -7,6 +10,11 @@ class CustomToast {
         fun plus(a:Int,b:Int):Int
         {
             return a+b;
+        }
+
+        fun showToast(context:Context,str:String)
+        {
+            Toast.makeText(context,str,Toast.LENGTH_SHORT).show();
         }
     }
 }
